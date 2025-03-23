0a531c0a43ef0a448a32398c153f9b2ee5a14e7f
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-03-23 01:47:03 +0100

    Added Android Studio Project. Added custom colors and logic to retrieve well being stats from my personal phone.

---
+++ app/src/main/java/com/example/cyberopoli/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.cyberopoli
+
+import android.app.AppOpsManager
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cyberopoli.ui.theme.CyberopoliTheme
+import java.util.Calendar
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!hasUsageStatsPermission(this)) {
+            requestUsageStatsPermission()
+        } else {
+            val usageStatsList = getMostUsedApps(this)
+            usageStatsList.take(5).forEach {
+                Log.d("APP_USAGE", "App: ${it.packageName} - Tempo in primo piano: ${it.totalTimeInForeground / 1000} sec")
+
+                val usageTimeMillis = getTodayUsageTime(this)
+                Log.d("USAGE_TIME", "Tempo totale di utilizzo oggi: ${usageTimeMillis / 1000} secondi")
+            }
+        }
+
+        enableEdgeToEdge()
+        setContent {
+            CyberopoliTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    private fun hasUsageStatsPermission(context: Context): Boolean {
+        val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            appOps.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), context.packageName)
+        } else {
+            appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), context.packageName)
+        }
+        return mode == AppOpsManager.MODE_ALLOWED
+    }
+
+    private fun requestUsageStatsPermission() {
+        startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+    }
+
+    private fun getMostUsedApps(context: Context): List<UsageStats> {
+        val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val endTime = System.currentTimeMillis()
+        val startTime = endTime - 24 * 60 * 60 * 1000 // Ultime 24 ore
+        val stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime)
+        return stats.sortedByDescending { it.totalTimeInForeground }
+    }
+
+    private fun getTodayUsageTime(context: Context): Long {
+        // Crea un'istanza di Calendar per impostare l'inizio della giornata
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val startTime = calendar.timeInMillis
+        val endTime = System.currentTimeMillis()
+
+        val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime)
+        val totalUsageTimeMillis = stats.sumOf { it.totalTimeInForeground }
+        return totalUsageTimeMillis
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CyberopoliTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/cyberopoli/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.cyberopoli.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val DarkColorsScheme = darkColorScheme(
+    primary = Color(0xFF222831),
+    onPrimary = Color.White,
+    secondary = Color(0xFF0D1E37),
+    onSecondary = Color.White,
+    tertiary = Color(0xFF8CFF00),
+)
+
+val LightColorsScheme = lightColorScheme(
+    primary = Color(0xFFC8C8C8),
+    onPrimary = Color.Black,
+    secondary = Color(0xFF1B1A55),
+    onSecondary = Color.Black,
+)
