1a57157410479d500bbe6990746d6cbe29cb4e4f
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-05-18 12:56:09 +0200

    Refactor LoadingScreen; enhance layout with Column for better structure, add loading text with appropriate styling, and update CircularProgressIndicator colors for improved theming.

--- Cyberopoli/app/src/main/java/com/unibo/cyberopoli/ui/screens/loading/LoadingScreen.kt
+++ Cyberopoli/app/src/main/java/com/unibo/cyberopoli/ui/screens/loading/LoadingScreen.kt
@@ -1,13 +1,21 @@
 package com.unibo.cyberopoli.ui.screens.loading
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.unibo.cyberopoli.R
 
 @Composable
 fun LoadingScreen() {
@@ -17,6 +25,19 @@ fun LoadingScreen() {
             .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                trackColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = stringResource(R.string.loading),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
     }
 }
